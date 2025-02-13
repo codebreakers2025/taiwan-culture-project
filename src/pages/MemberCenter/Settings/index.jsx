fb85870a2f9d81e22ba00dfa90bbcbff8ee60990
bennyhong <seanhong1215@gmail.com>
2025-02-13 22:49:26 +0800

    1. 調整手機選單 2. 會員中心 選單製作 3. 個人資料頁面及功能製作 4. css 版面調整

---
+++ src/pages/MemberCenter/Settings/index.jsx
@@ -0,0 +1,11 @@
+const Settings = () => {
+  return (
+    <div className="page-container">
+      <div className="content">
+        <h2>喜好設定...</h2>
+      </div>
+  </div>
+  );
+};
+
+export default Settings;
