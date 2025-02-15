6bf3497d07e57f4b65dbc25034eb3a6eb97ecb62
pencil <yuyu.hunter@gmail.com>
2025-02-15 23:13:38 +0800

    新增活動清單頁ActivityList結構及樣式

--- src/pages/ActivityList/index.jsx
+++ src/pages/ActivityList/index.jsx
@@ -1,10 +1,29 @@
+import { ActivityCard, ReviewCard, BlogCard} from '@/components/Card';
+import './ActivityList.scss';
+import { useTranslation } from 'react-i18next';
+import React, { useEffect, useState } from 'react';
+import { getActivity, getJournal, getReviews } from '@/utils/api';
+
 const About = () => {
     return (
       <div className="page-container">
         <div className="content">
-          <h1>正在開發中...</h1>
+          <div className="container">
+            <div className="row">
+              <div className="col-3">
+                <div className="left-searchBar">
+                  <div>col-3</div>
+                </div>
+              </div>
+              <div className="col-9">
+                <div>col-9
+                  <h1>正在開發中...</h1>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
+      </div>
     );
   };
   
